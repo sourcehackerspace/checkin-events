56cd384aadc318df3a011a07cb2a8bc3ea124dfb
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2017-02-16 17:18:08 -0600

    Modelo Courses Listo

---
+++ database/migrations/2017_02_16_225929_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('quota'); //total de lugares
+            $table->integer('busy'); //lugares ocupados
+            $table->integer('remaining'); //lugares restantes
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
